042dbf91c7fd1d546cc03327a326ad27d56d9c61
elliospizzaman <elliospizzaman@users.noreply.github.com>
2013-11-10 17:29:50 -0500

    Update plane.js

--- plane.js
+++ plane.js
@@ -1,4 +1,4 @@
-function createPlane(width, height, orientation){
+function createPlane(width: float, height : float, orientation){
 	/*Divide width and height by two to get to distance from the plane's centerpoint to edge*/
 	var halfW = width/2;
 	var halfH = height/2;
@@ -8,16 +8,16 @@ function createPlane(width, height, orientation){
 	if (orientation=="vertical"){
 		/*Define the vertices of the Mesh*/
 		meshObj.vertices = [Vector3(-halfW, -halfH, 0), Vector3(halfW, -halfH, 0), Vector3(halfW, halfH, 0), Vector3(-halfW, halfH, 0)];
+		/*Connect the vertices into two triangles that form a rectangular plane*/
+		meshObj.triangles = [0, 1, 2, 0, 2, 3];
 	} else{
 		/*Define the vertices of the Mesh*/
-		meshObj.vertices = [Vector3(-halfW, 0, -halfH), Vector3(halfW, 0, -halfH), Vector3(halfW, 0, halfH), Vector3(-halfW, 0, halfH)];
+		meshObj.vertices = [Vector3(-halfW, 0.1, -halfH), Vector3(halfW, 0.1, -halfH), Vector3(halfW, 0.1, halfH), Vector3(-halfW, 0.1, halfH)];
+		/*Connect the vertices into two triangles that form a rectangular plane*/
+		meshObj.triangles = [3, 2, 0, 2, 1, 0];
 	}
 	/*Assign the base texture coordinates of the mesh.*/
 	meshObj.uv = [Vector2 (0, 0), Vector2 (0, 1), Vector2(1, 1), Vector2 (1, 0)];
-	/*Connect the vertices into two triangles that form a rectangular plane*/
-	meshObj.triangles = [0, 1, 2, 0, 2, 3];
-	/*Recalculate the normals of the mesh from the triangles and vertices.*/
-	meshObj.RecalculateNormals();
 	/*Define a variable as a new gameobject with a MeshRenderer, MeshFilter, and MeshCollider attached to it.*/
 	/*Name the game object "plane"*/
 	var obj : GameObject = new GameObject("plane", MeshRenderer, MeshFilter, MeshCollider);
